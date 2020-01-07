d07fae94bf53bdc3e6b4578ac8418e92b57d0865
Andrea <Andrea@DESKTOP-FUV65OJ.homenet.telecomitalia.it>
2020-01-07 17:51:45 +0100

    Risoluzione della violazione di tipo cwe nel file MaxCore.java nel quale ichiedeva di definire un'eccezione più specifica nella riga 136

--- src/main/java/org/junit/experimental/max/MaxCore.java
+++ src/main/java/org/junit/experimental/max/MaxCore.java
@@ -133,7 +133,7 @@ public class MaxCore {
         }
         Class<?> type = each.getTestClass();
         if (type == null) {
-            throw new RuntimeException("Can't build a runner from description [" + each + "]");
+            throw new NullPointerException("Can't build a runner from description [" + each + "]");
         }
         String methodName = each.getMethodName();
         if (methodName == null) {
